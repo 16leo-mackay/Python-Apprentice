f5960aef411baa8817b97a2a6e9b6aa018d44e7f
16leo-mackay <leo.mackay@students.jointheleague.org>
2024-10-15 22:47:58 +0000

    almost made a pentagon

--- lessons/00_Turtles/05c_Loop_with_Turtle.py
+++ lessons/00_Turtles/05c_Loop_with_Turtle.py
@@ -1,11 +1,25 @@
 
-"""
-Turtles with a loop. 
 
-Study the previous program, 05a_Loop_with_Turtle.py, and then
-write a new program that uses a loop to draw a pentagon.
-( You can cut and past most of it! )
 
-"""
+
+
+import turtle                           
+turtle.setup(width=600, height=600)     
+
+tina=turtle.Turtle()
+
 tina.forward(50)
+tina.left (30)
+tina.forward(50)
+tina.left (30)
+tina.forward(50)
+tina.
+
+
+
+
+
+
+
+
 
